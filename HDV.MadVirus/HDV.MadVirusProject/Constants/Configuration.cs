0467f25376255bd27e4c8462dbb8b0b8d0260456
Vinh Nhon <thevinh92@gmail.com>
2015-03-19 07:23:12 -0700

    Merge VinhNhon code to HDV Repository

--- HDV.MadVirus/HDV.MadVirusProject/Constants/Configuration.cs
+++ HDV.MadVirus/HDV.MadVirusProject/Constants/Configuration.cs
@@ -12,5 +12,17 @@ namespace HDV.MadVirus.Constants
     public class Configuration
     {
         public const string FacebookAppId = "";
+
+        // Virus Size
+        public const float VIRUS_SPRITE_WIDTH = 84.0f;
+        public const float VIRUS_SPRITE_HEIGHT = 74.0f;
+
+        // Vitus Type
+        public const int BLUE_VIRUS_ID = 2;
+        public const int GREEN_VIRUS_ID = 4;
+        public const int MAGENTA_VIRUS_ID = 1;
+        public const int ORANGE_VIRUS_ID = 3;
+        public const int RED_VIRUS_ID = 6;
+        public const int YELLOW_VIRUS_ID = 5;
     }
 }
